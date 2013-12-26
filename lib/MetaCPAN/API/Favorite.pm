96aa6eeb0af45d39aa03f9bb8f3fd44e927150ac
Sawyer X <xsawyerx@cpan.org>
2013-12-26 13:51:27 +0100

    some data objects

--- lib/MetaCPAN/API/Favorite.pm
+++ lib/MetaCPAN/API/Favorite.pm
@@ -1,34 +1,24 @@
-use strict;
-use warnings;
 package MetaCPAN::API::Favorite;
-# ABSTRACT: Favorite ++ information for MetaCPAN::API
+# ABSTRACT: A Favorite data object
 
-use Carp;
-use Any::Moose 'Role';
+use Moo;
 
-# /favorite/_search only
-sub favorite {
-    my $self  = shift;
-    my %opts  = @_ ? @_ : ();
-    my $url   = '';
-    my $error = "Only 'search' can be used here";
+my @known_fields = qw<date user release id author distribution>;
 
-    %opts or croak $error;
-
-    my %extra_opts = ();
-
-    if ( defined ( my $search_opts = $opts{'search'} ) ) {
-        ref $search_opts && ref $search_opts eq 'HASH'
-            or croak $error;
-
-        %extra_opts = %{$search_opts};
-        $url        = 'favorite/_search';
-    } else {
-        croak $error;
-    }
-
-    return $self->fetch( $url, %extra_opts );
+foreach my $field (@known_fields) {
+    has $field => (
+        is      => 'ro',        
+        default => sub {
+            my $self = shift;
+            return $self->data->{$field};
+        },
+    );
 }
+
+has data => (
+    is       => 'ro',
+    required => 1,
+);
 
 1;
 
@@ -36,22 +26,6 @@ __END__
 
 =head1 DESCRIPTION
 
-This role provides MetaCPAN::API with fetching information about favorite
-++ information.
 
-=head1 METHODS
+=head1 ATTRIBUTES
 
-=head2 favorite
-
-    # example lifted from MetaCPAN docs
-    my $result = $mcpan->favorite(
-        search => {
-            user   => "SZABGAB",
-            fields => "distribution",
-            size   => 100,
-        },
-    );
-
-Searches MetaCPAN for favorite ++ information.
-
-Only complex searches are currently available.
--- lib/MetaCPAN/API/Rating.pm
+++ lib/MetaCPAN/API/Rating.pm
@@ -1,42 +1,27 @@
-use strict;
-use warnings;
 package MetaCPAN::API::Rating;
-# ABSTRACT: Rating information for MetaCPAN::API
+# ABSTRACT: A Rating data object
 
-use Carp;
-use Any::Moose 'Role';
+use Moo;
 
-# /rating/{id}
-# /rating/_search
-sub rating {
-    my $self  = shift;
-    my $url   = '';
-    my $error = "Either provide 'id' or 'search'";
+my @known_fields = qw<
+    date release author details
+    rating distribution helpful user
+>;
 
-    my %extra_opts = ();
-
-    if ( @_ == 1 ) {
-        $url = 'rating/' . shift;
-    } elsif ( @_ ) {
-        my %opts = @_;
-
-        if ( defined ( my $id = $opts{'id'} ) ) {
-            $url = "rating/$id";
-        } elsif ( defined ( my $search_opts = $opts{'search'} ) ) {
-            ref $search_opts && ref $search_opts eq 'HASH'
-                or croak $error;
-
-            %extra_opts = %{$search_opts};
-            $url        = 'rating/_search';
-        } else {
-            croak $error;
-        }
-    } else {
-        croak $error;
-    }
-
-    return $self->fetch( $url, %extra_opts );
+foreach my $field (@known_fields) {
+    has $field => (
+        is      => 'ro',        
+        default => sub {
+            my $self = shift;
+            return $self->data->{$field};
+        },
+    );
 }
+
+has data => (
+    is       => 'ro',
+    required => 1,
+);
 
 1;
 
@@ -44,25 +29,6 @@ __END__
 
 =head1 DESCRIPTION
 
-This role provides MetaCPAN::API with fetching information about CPAN
-ratings.
 
-=head1 METHODS
+=head1 ATTRIBUTES
 
-=head2 rating
-
-    my $result = $mcpan->rating( id => 'UC6tqabqR-y3xxZk0tgVXQ' );
-
-Searches MetaCPAN for CPAN ratings.  
-
-You can do complex searches using 'search' parameter:
-    
-    my $result = $mcpan->rating(
-        search => {
-            filter => "distribution:Moose",
-            fields => [ "date", "rating" ],
-        },
-    );
-
-These searches will give you the right _id to use for more detailed
-information.
