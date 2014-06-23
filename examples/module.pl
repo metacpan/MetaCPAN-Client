ee7f49a20198878f74bad2563e83dff7d36f3a30
mickey <mickey75@gmail.com>
2014-06-23 20:40:48 +0200

    yet another example (module)

---
+++ examples/module.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use DDP;
+
+use MetaCPAN::Client;
+
+my $module =
+    MetaCPAN::Client->new->module('Moo');
+
+my %output = (
+    NAME        => $module->name,
+    ABSTRACT    => $module->abstract,
+    DESCRIPTION => $module->description,
+    RELEASE     => $module->release,
+    AUTHOR      => $module->author,
+    VERSION     => $module->version,
+);
+
+p %output;
+
