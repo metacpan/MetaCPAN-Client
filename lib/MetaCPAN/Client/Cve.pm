31b3d3b99bec652468f1c436b712b055d18bc88c
Michal Josef Špaček <mspacek@redhat.com>
2026-06-24 10:17:03 +0200

    Improve documentation related to work with CVEs

--- lib/MetaCPAN/Client/Cve.pm
+++ lib/MetaCPAN/Client/Cve.pm
@@ -35,11 +35,77 @@ __END__
 
 =head1 SYNOPSIS
 
+    # Get a specific CVE by CPANSA ID
     my $cve = $mcpan->cve('CPANSA-DBD-SQLite-2019-5018');
+    print $cve->distribution;  # 'DBD-SQLite'
+    print $cve->severity;      # e.g., 'high'
+
+    # Search for CVEs by distribution
+    my $cve_resultset = $mcpan->cve({ distribution => 'DBD-SQLite' });
+    while (my $cve = $cve_resultset->next) {
+        print $cve->cpansa_id, "\n";
+    }
+
+    # Search for CVEs by severity
+    my $cve_resultset = $mcpan->cve({ severity => 'high' });
+
+    # Search for CVEs by CVE ID - requires all() with ElasticSearch query
+    my $by_cve_id_resultset = $mcpan->all('cve', { es_filter => { 'match_phrase' => { 'cves' => 'CVE-2026-5083' } } });
+    while (my $cve = $by_cve_id_resultset->next) {
+        print $cve->cpansa_id, "\n";
+    }
+
+    # Get all CVEs
+    my $all_cves = $mcpan->all('cve');
 
 =head1 DESCRIPTION
 
-A MetaCPAN CVE entity object.
+A MetaCPAN CVE (CPAN Security Advisory) entity object.
+
+=head2 How to get CVE objects
+
+There are several ways to retrieve CVE objects:
+
+=over 4
+
+=item 1. By CPANSA ID (returns single object)
+
+    my $cve = $mcpan->cve('CPANSA-DBD-SQLite-2019-5018');
+
+The CPANSA ID format is: C<CPANSA-DistributionName-YYYY-NNNNN>
+
+=item 2. By search criteria (returns ResultSet)
+
+    # Search by distribution name (use dashes, not ::)
+    my $cve_resultset = $mcpan->cve({ distribution => 'YAML-Syck' });
+
+    # Search by severity
+    my $cve_resultset = $mcpan->cve({ severity => 'high' });
+
+    # Search by reported date
+    my $cve_resultset = $mcpan->cve({ reported => '2024-01-01' });
+
+Then iterate through results:
+
+    while (my $cve = $cve_resultset->next) {
+        # process each CVE
+    }
+
+=item 3. By CVE ID (requires ElasticSearch query)
+
+B<Note:> The C<cves> field is not a supported search parameter for the C<cve()> method.
+To search by CVE ID, use C<all()> with an ElasticSearch query:
+
+    my $cve_resultset = $mcpan->all('cve', { es_filter => { 'match_phrase' => { 'cves' => 'CVE-2026-5083' } } });
+    while (my $cve = $cve_resultset->next) {
+        print $cve->cpansa_id, "\n";
+    }
+
+=item 4. All CVEs
+
+    my $all_cve_resultset = $mcpan->all('cve');
+
+=back
 
 =head1 ATTRIBUTES
 
